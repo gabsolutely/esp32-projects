32d26e6dbfdd0d44e19fc2fd4b720e4a8c213cf9
Gab <gabrianronsairo@gmail.com>
2025-09-25 00:09:24 +0800

    Create Day1-BlinkingLEDs.ino

---
+++ Day1-BlinkingLEDs.ino
@@ -0,0 +1,28 @@
+// put your ledPins here
+int ledPin1 = 10;
+int ledPin2 = 10;
+int ledPin3 = 21;
+int dt = 200;
+
+void setup() {
+// Put your setup code here, to run once:
+pinMode (ledPin1, OUTPUT);
+pinMode (ledPin2, OUTPUT);
+pinMode (ledPin3, OUTPUT);
+}
+void loop() {
+// Put your main code here, to run repeatedly:
+digitalWrite (ledPin1, HIGH);
+digitalWrite (ledPin2, LOW);
+digitalWrite (ledPin3, LOW);
+delay (dt);
+
+digitalWrite (ledPin1, LOW);
+digitalWrite (ledPin2, HIGH);
+digitalWrite (ledPin3, LOW);
+delay (dt);
+
+digitalWrite (ledPin1, LOW);
+digitalWrite (ledPin2, LOW);
+digitalWrite (ledPin3, HIGH);
+delay (dt);
